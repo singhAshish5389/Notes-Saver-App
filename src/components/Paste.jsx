718140ad74a67dc8ae2ba9274f14e70991c29f25
Ashish Singh <singhashish12000@gmail.com>
2025-09-05 16:16:51 +0530

    Width Issue Resolved

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -24,17 +24,18 @@ function Paste() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col">
       <input
-        className="mt-3 min-h-1.5 max-w-5xl min-w-xs hover:border-green-800 w-full px-4 py-1.5 rounded-2xl border h-11 border-gray-300 
+        className="mt-3  min-h-1.5 max-w-5xl min-w-xs hover:border-green-800 w-full px-4 py-1.5 rounded-2xl border h-11 border-gray-300 
          focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
          placeholder-gray-400 text-gray-700 shadow-sm"
         type="search"
         value={searchTerms}
+        maxLength={35}
         onChange={(e) => setsearchTerms(e.target.value)}
       />
       <div
-        className="w-[65vw] display-flex flex-col gap-2 mt-3 p-3 border border-gray-300 rounded-xl 
+        className="min-w-4xl display-flex flex-col gap-2 mt-3 p-3 border border-gray-300 rounded-xl 
          focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
          resize-none "
       >
@@ -43,23 +44,22 @@ function Paste() {
           filterdData.map((paste, index) => {
             return (
               <div
-                className="bg-white shadow-md rounded-2xl min-h-[20vh] flex mt-2 mb-2 p-4 border border-gray-200 
-                hover:shadow-lg transition-shadow duration-200 
-                w-full max-w-[100]"
-                key={paste.id + "-" + index}
-              >
-                <div className="flex flex-col max-w-[80%] min-w-[70%]">
-                  <div className="text-lg text-left ml-1 mr-1 pl-2 border-1 font-semibold bg-blue-200 text-emerald-950 break-words">
+                className="bg-white shadow-md rounded-2xl min-h-[20vh] flex flex-row justify-between items-start p-4 border border-gray-200 
+  hover:shadow-lg transition-shadow duration-200 min-w-3xl max-w-5xl"
+                key={paste.id + "-" + index}>
+                <div className="bg-white shadow-md rounded-2xl min-h-[20vh] flex flex-col justify-between items-start p-4 border border-gray-200 
+  hover:shadow-lg transition-shadow duration-200 w-[85%]">
+                  <div className="text-lg text-left ml-1 mr-1 pl-2 border-1 font-semibold bg-blue-200 text-emerald-950 break-words ">
                     Title:{paste.title}
                   </div>
-                  <div className="text-sm text-left text-blue-400 break-words ">
+                  <div className="text-sm text-left w-[100%] text-blue-400 break-words ">
                     Content:{paste.content}
                   </div>
-                  <div className="text-xs  flex items-baseline-last text-red-300">
+                  <div className="text-xs  flex items-baseline-last  text-red-300">
                     {paste.createdAt}
                   </div>
                 </div>
-                <div className="ml-5 grid grid-cols-2 gap-2">
+                <div className="ml-5 grid grid-cols-2 gap-2 min-w-[12%]">
                   <button className="flex items-center gap-2 px-3 py-2 rounded-lg 
   bg-gray-600 text-white text-sm font-medium 
   hover:bg-green-700 shadow-sm hover:shadow-md 
